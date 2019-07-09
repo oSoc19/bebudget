20e1b3f4b56f895dc3a4d31a8ef1ec4f5b281225
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-07-09 13:52:17 +0200

    fonts chosen

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -17,7 +17,7 @@
 
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|IBM+Plex+Sans+Condensed|Imprima|Julius+Sans+One|Mallanna|Mandali|Noto+Sans+HK|Nunito|Overpass|PT+Sans|Padauk|Pavanam|Poppins|Raleway|Signika|Spinnaker|Ubuntu|Varela+Round|Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito|Poppins&display=swap" rel="stylesheet">
 
 	<script>
 		var site_url = '<?php echo site_url(); ?>';
