f7423ffd25b04bb00ba9376953727bcfa14cb0df
Bram Bleys <b.bleys@hotmail.com>
2019-07-10 16:08:33 +0200

    Backend of quiz with simple styling

--- application/views/quiz_view.php
+++ application/views/quiz_view.php
@@ -1,11 +1,9 @@
 <?php
 
-    foreach ($questions as $question) {
-        $count = 0;
+    foreach ($questions as $key => $question) {
         ?>
         <div class='card card-custom' style='width: 75vw;'>
             <div class='card-body card-body-custom'>
-                <?php echo form_open('quiz/checkAnswer', array('onsubmit' => 'return checkAnswer()', 'id' => "question-$count")); ?>
                 <h2 class='card-title card-title-custom'><?php echo $question->question ?></h2>
                 <div class="buttons">
                     <?php
@@ -17,27 +15,63 @@
                                     'value' => $answer->value,
                                     'type' => 'submit',
                                     'content' => ucfirst($answer->name),
-                                    'class' => 'answer-button',
-                                    'onclick' => 'checkAnswer'
+                                    'class' => 'answerButton'
+                                );
+                            } else {
+                                $data = array(
+                                    'name' => 'givenAnswer',
+                                    'value' => $answer,
+                                    'type' => 'submit',
+                                    'content' => $answer,
+                                    'class' => 'answerButton'
                                 );
                             }
                             echo form_button($data);
                         }
                     ?>
 
-                    <input type="hidden" name="correctAnswer" value="<?php echo $question->correctAnswer; ?>">
+                    <input type="hidden" id="correctAnswer" value="<?php echo $question->correctAnswer; ?>">
+                    <input type="hidden" id="questionNumber" value="<?php echo $key; ?>">
                 </div>
-                <?php echo form_close(); ?>
+
+                <label><?php echo $question->correctAnswer; ?></label>
+                <label class="score"></label>
+                <div class="alert alert-success alert-success-custom hidden question-success-<?php echo $key; ?>" role="alert">
+                    Well done, that is the correct answer!
+                </div>
+                <div class="alert alert-danger alert-danger-custom hidden question-danger-<?php echo $key; ?>" role="alert">
+                    Unfortunately, that is not the right answer...
+                </div>
+                <a href="#" class="hidden readMore">Want to read more? ></a>
             </div>
         </div>
     <?php } ?>
 
 <script>
 
+    let score = 0;
 
-    function checkAnswer() {
-        console.log('hi');
+    $(function () {
+        $('.score').text("Score: " + score);
 
-        return false;
-    }
+        $('.answerButton').on('click', function () {
+            let givenAnswer = $(this).val();
+            let correctAnswer = $(this).parent().find('#correctAnswer').val();
+            let questionNumber = $(this).parent().find('#questionNumber').val();
+
+            if (givenAnswer === correctAnswer) {
+                score++;
+                $('.score').text("Score: " + score);
+                $('.question-success-' + questionNumber).removeClass('hidden');
+            } else {
+                $('.question-danger-' + questionNumber).removeClass('hidden');
+            }
+
+            $(this).parent().children().attr("disabled", true);
+            $(this).parent().parent().find('.readMore').removeClass('hidden');
+
+            //TODO scroll up animation and show next question
+        });
+    });
+
 </script>
