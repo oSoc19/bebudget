533d187ab6a7c7deb9d82a082f7335cdff140168
Bram Bleys <b.bleys@hotmail.com>
2019-07-10 11:47:26 +0200

    Temporary commit

--- application/models/Quiz_model.php
+++ application/models/Quiz_model.php
@@ -6,15 +6,19 @@
         public function __construct() {
             parent::__construct();
 
-            $url = 'http://84.195.17.11/api/2017.json';
-            $this->data = json_decode(file_get_contents($url));
+            //            $url = 'http://84.195.17.11/api/2017.json';
+            //            $this->data = json_decode(file_get_contents($url));
+            $this->data = json_decode(file_get_contents('./uploads/2017.json'));
 
             $this->load->helper('form');
         }
 
         public function generateQuestions() {
 
-            //TODO change this value
+            $numberOfQuestions = 5;
+            //TODO for loop for $numberOfQuestions
+
+            //TODO change this value to:
             // $random = rand(0, 4);
             $random = 0;
             $question = new stdClass();
@@ -22,10 +26,33 @@
 
             switch ($random) {
                 case 0:
-                    $answers = $this->generateCategories();
-                    $question->question = "Where does the government spend most?";
-                    $question->answers = $answers->categories;
-                    $question->correctAnswer = $answers->correctAnswer;
+                    $data = $this->generateThreeCategories();
+                    $question->question = 'Where does the government spend most?';
+                    $question->answers = $data->categories;
+                    $question->correctAnswer = $data->correctAnswer;
+
+                    $questions[] = $question;
+                    break;
+                case 1:
+                    $data = $this->generateCategoryWithAnswers();
+                    $question->question = 'How much money is spent on ' . $data->category . '?';
+                    $question->answers = $data->answers;
+                    $question->correctAnswer = $data->correctAnswer;
+
+                    $questions[] = $question;
+                    break;
+                case 2:
+                    $data = $this->generateCategoryWithAnswersInPercentages();
+                    $question->question = 'What percentage of government expenditure goes to ' . $data->category . '?';
+                    $question->answers = $data->answers;
+                    $question->correctAnswer = $data->correctAnswer;
+
+                    $questions[] = $question;
+                    break;
+                case 3:
+                    $data = $this->generateTwoCategories();
+                    $question->question = 'More is spent on ' . $data->firstCategory . ' than ' . $data->secondCategory . '.';
+                    $question->correctAnswer = $data->correctAnswer;
 
                     $questions[] = $question;
                     break;
@@ -35,36 +62,89 @@
         }
 
         // Helper function to generate 3 random categories
-        private function generateCategories() {
+        private function generateThreeCategories() {
             $categories = array();
             $previousCategories = array();
             $values = array();
-            $answers = new stdClass();
+            $data = new stdClass();
 
             for ($i = 0; $i < 3; $i++) {
-                $random = str_pad(rand(1, count((array)$this->data) - 1), 2, '0', STR_PAD_LEFT);
+                // Generate a random number within the length of the categories array
+                $random = rand(0, count((array)$this->data->{'TOT'}->categories) - 1);
 
+                // Check if the random number is already in the array so it can't pick the same category twice
                 if (!in_array($random, $previousCategories, TRUE)) {
                     // Pick a random category
-                    $category = $this->data->{'F' . $random};
+                    $category = $this->data->{'TOT'}->categories[$random];
+
+                    // Get rid of the numbers in the name
                     $category->name = $this->removeNumbersFromName($category->name);
+
+                    // Add the random category to the array and save its value
                     $categories[] = $category;
                     $values[] = $category->value;
+
+                    // Add random category to previousCategory array so it can't be picked again
                     $previousCategories[] = $random;
                 } else {
+                    // Pick a new random number if it was already in the array
                     $i--;
                 }
             }
 
-            $answers->categories = $categories;
-            $answers->correctAnswer = max($values);
-            return $answers;
+            $data->categories = $categories;
+            $data->correctAnswer = max($values);
+            return $data;
+        }
+
+        // Helper function to generate 1 random category with 3 possible answers
+        private function generateCategoryWithAnswers() {
+            $data = new stdClass();
+            $answers = array();
+
+            $random = rand(0, count((array)$this->data->{'TOT'}->categories) - 1);
+            $category = $this->data->{'TOT'}->categories[$random];
+
+            $category->name = $this->removeNumbersFromName($category->name);
+
+            $minimum = $category->value - (0.7 * $category->value);
+            $maximum = $category->value + (0.7 * $category->value);
+
+            $answer1 = rand($minimum, $maximum);
+            $answer2 = rand($minimum, $maximum);
+
+            $answers[] = (int)$category->value;
+            $answers[] = $answer1;
+            $answers[] = $answer2;
+
+            shuffle($answers);
+
+            $data->category = $category->name;
+            $data->answers = $answers;
+            $data->correctAnswer = $category->value;
+
+            return $data;
+        }
+
+        // Helper function to generate 1 random category with 3 possible answers in percentages
+        private function generateCategoryWithAnswersInPercentages() {
+
+        }
+
+        // Helper function to generate 2 random categories (answers not included because of true or false)
+        private function generateTwoCategories() {
+
         }
 
         // Helper function to remove the numbers from the name of a category
         private function removeNumbersFromName($name) {
+            //Split up the words of the name into an array
             $parts = explode(' ', $name);
+
+            // Remove the first element of the array, which will be the number
             array_shift($parts);
+
+            // Put the name back together and return it
             return implode(' ', $parts);
         }
     }
--- application/views/quiz_view.php
+++ application/views/quiz_view.php
@@ -1,30 +1,43 @@
 <?php
 
     foreach ($questions as $question) {
+        $count = 0;
         ?>
         <div class='card card-custom' style='width: 75vw;'>
             <div class='card-body card-body-custom'>
-                <?php echo form_open('quiz/checkAnswer'); ?>
+                <?php echo form_open('quiz/checkAnswer', array('onsubmit' => 'return checkAnswer()', 'id' => "question-$count")); ?>
                 <h2 class='card-title card-title-custom'><?php echo $question->question ?></h2>
                 <div class="buttons">
                     <?php
                         $attributes = array('class' => 'answer-button');
                         foreach ($question->answers as $answer) {
-                            $data = array(
-                                'name' => 'givenAnswer',
-                                'id' => $answer->id,
-                                'value' => $answer->name,
-                                'type' => 'submit',
-                                'content' => ucfirst($answer->name),
-                                'class' => 'answer-button'
-                            );
-
+                            if (is_object($answer)) {
+                                $data = array(
+                                    'name' => 'givenAnswer',
+                                    'value' => $answer->value,
+                                    'type' => 'submit',
+                                    'content' => ucfirst($answer->name),
+                                    'class' => 'answer-button',
+                                    'onclick' => 'checkAnswer'
+                                );
+                            }
                             echo form_button($data);
                         }
                     ?>
 
                     <input type="hidden" name="correctAnswer" value="<?php echo $question->correctAnswer; ?>">
                 </div>
+                <?php echo form_close(); ?>
             </div>
         </div>
     <?php } ?>
+
+<script>
+
+
+    function checkAnswer() {
+        console.log('hi');
+
+        return false;
+    }
+</script>
