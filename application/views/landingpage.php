3f1a7492858b29a5694be2f72e2aaa629dfefc23
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-07-16 13:51:37 +0200

    fixed issue #21

--- application/views/landingpage.php
+++ application/views/landingpage.php
@@ -1,18 +1,14 @@
 <header class="masthead">
     <div class="container h-100">
-       <div class="container" id="lang">
+        <div class="container" id="lang">
             <a href="#">NL</a>
             <a href="#">EN</a>
             <a href="#">FR</a>
         </div>
         <div class="container" id="nav">
-            <?php echo anchor("","Home"); ?>
-            <?php /*echo anchor("#graph","Info"); */?>
-            <a href="#graph">Info</a>
-            <?php /*echo anchor("#quiz","Quiz"); */?>
-            <a href="#quiz">Quiz</a>
-            <?php /*echo anchor("","Contact"); */?>
-            <a href="#" data-toggle="modal" data-target="#upload-form"></a>
+            <?php echo anchor("", "Home"); ?>
+            <a href="#quiz" class="js-scroll-trigger">Quiz</a>
+            <a href="#graph" class="js-scroll-trigger">Info</a>
         </div>
         <div class="row h-100 align-items-center">
             <div class="col-12 text-center">
