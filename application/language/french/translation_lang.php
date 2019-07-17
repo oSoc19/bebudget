f7c365bcb1da9ccf5e95cea026ac482c0a7740a3
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-07-17 09:59:16 +0200

    questions & footer french

--- application/language/french/translation_lang.php
+++ application/language/french/translation_lang.php
@@ -4,15 +4,21 @@ $lang["landingpage_btn_quiz"] = "Oui, ...";
 $lang["landingpage_btn_chart"] = "Non, ...";
 
 // Quiz
-/*$lang["quiz_title"] = "Test yourself with these <span class='highlight'>5 questions</span>";
-$lang["quiz_question1"] = "Where does the government spend most?";
-$lang["quiz_question2"] = "How much money is spent on ";
-$lang["quiz_question3"] = "What percentage of government expenditure goes to ";
-$lang["quiz_question4_first"] = "More is spent on ";
-$lang["quiz_question4_second"] = " than ";
-$lang["quiz_score"] = "Your score is: ";*/
+$lang["quiz_title"] = "Testez-vous avec ces <span class='highlight'>5 questions</span>!";
+$lang["quiz_question1"] = " Où le gouvernement dépense-t-il plus ?";
+$lang["quiz_question2"] = "Combien d’argent est consacré à la ";
+$lang["quiz_question3"] = "Quel est le pourcentage des dépenses publiques va à la ";
+$lang["quiz_question4_first"] = "Plus est consacré à la ";
+$lang["quiz_question4_second"] = " que ";
+$lang["quiz_score"] = "Votre score est: ";
 $lang["quiz_file"] = "data_french.json";
 
 // Chart
 $lang["chart_file"] = "data_french.json";
 $lang["chart_title"] = "Dépenses par catégorie, en pourcentage du total des dépenses publiques";
+
+
+// Footer
+$lang["footer_part1"] = " Les données utilisées dans ce site Web provient de l’année 2017.";
+$lang["footer_part2"] = " Dernière mise à jour sur: ";
+$lang["footer_part3"] = '<a href="#" data-toggle="modal" data-target="#upload-form">Télécharger les nouvelles données</a>';
