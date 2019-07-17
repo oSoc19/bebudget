cc58a260bd8ae9fd7d4dcbe2e13027dc32c587f7
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-07-17 11:13:29 +0200

    dt mistake in dutch translation

--- application/language/dutch/translation_lang.php
+++ application/language/dutch/translation_lang.php
@@ -7,7 +7,7 @@ $lang["landingpage_btn_chart"] = "Nee, ontdek meer";
 // Quiz
 $lang["quiz_title"] = "Test jezelf met deze <span class='highlight'>5 vragen</span>";
 $lang["quiz_question1"] = "Waaraan spendeert de overheid het meest?";
-$lang["quiz_question2"] = "Hoeveel wordt er gespendeert aan ";
+$lang["quiz_question2"] = "Hoeveel wordt er gespendeerd aan ";
 $lang["quiz_question3"] = "Hoeveel procent van de uitgaves van de overheid gaat naar ";
 $lang["quiz_question4_first"] = "Er wordt meer uitgegeven aan ";
 $lang["quiz_question4_second"] = " dan aan ";
