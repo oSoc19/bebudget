fcbb23c204258790d98d963ccb062598fafdc571
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-07-23 10:43:16 +0200

    fixed file to get quiz data from

--- application/models/Quiz_model.php
+++ application/models/Quiz_model.php
@@ -6,7 +6,7 @@
         public function __construct() {
             parent::__construct();
 
-            $this->data = json_decode(file_get_contents('./uploads/' .$this->lang->line("chart_file")));
+            $this->data = json_decode(file_get_contents('./uploads/' .$this->lang->line("quiz_file")));
             $this->usedCategories = array();
 
             $this->load->helper('form');
